593840db1131ae5e69391d7292be7e51770581fb
Stanislav Mankovskyi <odnorazka25@gmail.com>
2022-09-16 12:40:26 +0100

    short-button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GlobalStyle } from './components/GlobalStyle.jsx';
-import './index.css';
+// import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
